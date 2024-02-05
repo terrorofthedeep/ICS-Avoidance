fa82ac7fd006bc48ce47e7eb8645d558cd9aa919
terrorofthedeep <lucasmpierce@gmail.com>
2024-02-05 14:07:05 -0500

    Reworked existing code
    - Removed while loop from void loop(), to avoid unnecessary nested loop
    - Moved the code for checking each laser for potholes from loop() into separate functions called in loop

--- double_infrared.ino
+++ double_infrared.ino
@@ -1,20 +1,21 @@
 #include <SoftwareSerial.h>
 //If the laser gives a reading above this distance, we recognize a pothole
 const float maxLaserDistance = 1.11;
+const int waitTime = 1000; //How long in milliseconds we wait before checking for a pothole again
 
-const int rxPin1 2
-const int txPin1 3
-const int rxPin2 4
-const int txPin2 5
+const int rxPin1 = 2;
+const int txPin1 = 3;
+const int rxPin2 = 4;
+const int txPin2 = 5;
 SoftwareSerial mySerial_1 = SoftwareSerial(rxPin1, txPin1); // RX, TX
 SoftwareSerial mySerial_2 = SoftwareSerial(rxPin2, txPin2);   // RX, TX
 
-char buff[4]={0x80,0x06,0x03,0x77};
+//The numbers were taken from online, not sure what they do exactly
+char buff[4]={0x80,0x06,0x03,0x77}; //Hex for 128, 6, 3, 119
 unsigned char data_laser_1[11]={0};
 unsigned char data_laser_2[11]={0};
 
 void setup() {
-
   pinMode(rxPin1, INPUT);
   pinMode(txPin1, OUTPUT);
   pinMode(rxPin2, INPUT);
@@ -26,56 +27,65 @@ void setup() {
 }
 
 void loop() {
+  //These print statements are from the original code, but they don't seem to be necessary?
+  //mySerial_1.print(buff);
+  //mySerial_2.print(buff);
 
-  mySerial_1.print(buff);
-  mySerial_2.print(buff);
-  while(1){ 
-    mySerial_1.listen();
-    delay(1000);
-    if(mySerial_1.available()>0) 
-    {
-      
-      delay(50);
-      for(int i=0;i<11;i++){
-        data_laser_1[i]=mySerial_1.read();
-      }
-      
-      unsigned char Check=0;
-      
-      for(int i=0;i<10;i++){
-        Check=Check+data_laser_1[i];
-      }
-      
-      Check=~Check+1;
-      
-      if(data_laser_1[10]==Check){
-        if(data_laser_1[3]=='E'&& data_laser_1[4]=='R' && data_laser_1[5]=='R'){
-          Serial.println("Out of range");
-        }
-        else{
+
+  checkLaserOne();
+  checkLaserTwo();
+}
+
+
+
+void checkLaserOne() {
+  mySerial_1.listen();
+  delay(waitTime);
+  if(mySerial_1.available()>0) 
+  {
+
+    delay(50);
+    for(int i=0;i<11;i++){
+      data_laser_1[i]=mySerial_1.read();
+    }
         
-          float distance0 = 0;
-          distance0=(data_laser_1[3]-0x30)*100+(data_laser_1[4]-0x30)*10+(data_laser_1[5]-0x30)*1+(data_laser_1[7]-0x30)*0.1+(data_laser_1[8]-0x30)*0.01+(data_laser_1[9]-0x30)*0.001;
-          Serial.println("---------------");
-
-          if(distance0 > maxLaserDistance) 
-          {
-            Serial.println("POTHOLE DETECTED (R)");
-          }
-          else 
-          {
-            Serial.println("No pothole detected on Right");
-          }
-        }
+    unsigned char Check = 0;
+        
+    for(int i=0;i<10;i++){
+      Check=Check+data_laser_1[i];
+    }
+        
+    Check=~Check+1;
+    
+    if(data_laser_1[10]==Check){
+      if(data_laser_1[3]=='E'&& data_laser_1[4]=='R' && data_laser_1[5]=='R'){
+        Serial.println("Out of range");
       }
       else{
-        Serial.println("Invalid Data!");
+      
+        float distance0 = 0;
+        distance0 = (data_laser_1[3]-0x30)*100+(data_laser_1[4]-0x30)*10+(data_laser_1[5]-0x30)*1+(data_laser_1[7]-0x30)*0.1+(data_laser_1[8]-0x30)*0.01+(data_laser_1[9]-0x30)*0.001;
+        Serial.println("---------------");
+
+        if(distance0 > maxLaserDistance) 
+        {
+          Serial.println("POTHOLE DETECTED (R)");
+        }
+        else 
+        {
+          Serial.println("No pothole detected on Right");
+        }
       }
-    } 
-    
-    
-    mySerial_2.listen();
-    delay(1000);
+    }
+    else{
+      Serial.println("Invalid Data!");
+    }
+  }
+}
+
+void checkLaserTwo() {
+  mySerial_2.listen();
+    delay(waitTime);
     if(mySerial_2.available()>0) 
     {
       
@@ -118,5 +128,8 @@ void loop() {
         Serial.println("Invalid Data for mySerial_2");
       }
     }
-  }
 }
+
+
+
+
