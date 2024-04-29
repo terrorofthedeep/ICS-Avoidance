7a7ac98130965c03d1a7741d79dd6e37ccd2f437
terrorofthedeep <lucasmpierce@gmail.com>
2024-04-29 10:32:47 -0400

    Removed having to wait for ready char from user

--- simple_gyro.ino
+++ simple_gyro.ino
@@ -67,12 +67,6 @@ void setup() {
     // verify connection
     Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
 
-    // wait for ready
-    //Serial.println(F("\nSend any character to begin DMP programming and demo: "));
-    while (Serial.available() && Serial.read()); // empty buffer
-    while (!Serial.available());                 // wait for data
-    while (Serial.available() && Serial.read()); // empty buffer again
-
     // load and configure the DMP
     //Serial.println(F("Initializing DMP..."));
     devStatus = mpu.dmpInitialize();
