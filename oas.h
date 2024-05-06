5be08bb06cc4fcbfc9f5ef02e90dbffaaba10329
terrorofthedeep <lucasmpierce@gmail.com>
2024-05-06 10:20:39 -0400

    Updates from john

--- oas.h
+++ oas.h
@@ -10,14 +10,6 @@
 ///       OAS        ///
 ////////////////////////
 
-// ---------- Infra-Red initial variable declarations -----------------
-char buff[4] = {0x80, 0x06, 0x03, 0x77};
-unsigned char data_laser_1[11] = {0}; // Holds sensor data
-unsigned char data_laser_2[11] = {0};
-int distance_IR_1 = 0;
-int distance_IR_1 = 0;
-// --------------------------------------------------------------------
-
 // ---------- Ultra-Sonic initial variable declarations ---------------
 const int frontMaxDist = 300; // cm
 const int sideMaxDist = 30; // cm
@@ -38,58 +30,24 @@ const int echoPin_4 = 28;
 const int echoPin_5 = 30;
 const int echoPin_6 = 32;
 
-// Create sonar class attributed to each indivdual sensor
-// Most left is sonar_1 and Most Right is sonar_6
-NewPing sonar_1(trigPin_g, echoPin_1, sideMaxDist);
-NewPing sonar_2(trigPin_w, echoPin_2, frontMaxDist);
-NewPing sonar_3(trigPin_y, echoPin_3, frontMaxDist);
-NewPing sonar_4(trigPin_o, echoPin_4, frontMaxDist);
-NewPing sonar_5(trigPin_b, echoPin_5, frontMaxDist);
-NewPing sonar_6(trigPin_p, echoPin_6, sideMaxDist);
+const int ARRAY_SIZE = 100;
+const int MAX_SPEED = 255;
+const float maxLaserDistance = 1.11;
 
-// Array to store US data
-int distance_US[6] = {0};
 // --------------------------------------------------------------------
 
 // --------------------- MPU6050 variable setup -----------------------
 #define T_INT .01 // 10 ms --> .01 s
-MPU6050 mpu;      // MPU 6050 object
 
-// MPU control/status vars
-bool dmpReady = false;  // set true if DMP init was successful
-uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
-uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
-uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
-uint16_t fifoCount;     // count of all bytes currently in FIFO
-uint8_t fifoBuffer[64]; // FIFO storage buffer
+void avoid_setup();
 
-// other Variables
-float ax = 0;
-float ay = 0;
-float Vx, Vo_x = 0;
-float Vy, Vo_y = 0;
-float Px, Po_x = 0;
-float Py, Po_y = 0;
-float currBC[2] = {0};
-
-// orientation/motion vars
-Quaternion q;           // [w, x, y, z]         quaternion container
-VectorInt16 aa;         // [x, y, z]            accel sensor measurements
-VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
-VectorFloat gravity;    // [x, y, z]            gravity vector
-float euler[3];         // [psi, theta, phi]    Euler angle container
-float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
-// --------------------------------------------------------------------
-
-void setup();
-
-int check_IR(HardwareSerial& mySerial);
+int check_IR(HardwareSerial& mySerial, unsigned char laserData[]);
 
 void check_US();
 
-void detectAboveObstacles();
+void detectAboveObstacles(int &angle, int &speed);
 
-void detectIngroundObstacles();
+void detectIngroundObstacles(int &angle, int &speed);
 
 void check_gyro();
 
@@ -97,10 +55,10 @@ float getPosition(float Po, float  Vel, float Acc);
 
 float getVelocity(float Acc, float Vo);
 
-void trackMovement();
+void trackMovement(int &angle, int &speed);
 
-void updateYawPitchRoll(float deltaYaw, float deltaPitch, float deltaRoll)
+void updateYawPitchRoll(float deltaYaw, float deltaPitch, float deltaRoll);
 
-void updateVelocity(float deltaVx, float deltaVy)
+void updateVelocity(float deltaVx, float deltaVy);
 
-void avoid();
+void avoid(int &angle, int &speed);
